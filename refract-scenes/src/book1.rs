54f28948eee79db2f357e96324d70509a12583b1
Ashley Ingram <ashley.ingram@outlook.com>
2026-07-25 23:05:40 +0100

    Create a crate to contain scene samples
    Create the "refract-scenes" crate to contain sample scenes, like the one from the cover of book1. This will reduce duplication of scene definitions between the ui and the cli.
    
    For now, the book1 scene is still duplicated in core refract library main.rs, because using refract-scenes would introduce a circular dependency (scenes is dependent on the core, because it uses lots of the core type definitions like Circle, Color, etc.). This will be fixed by introducing a refract-cli crate, which is the equivalent of refract-ui (but for the command line).

---
+++ refract-scenes/src/book1.rs
@@ -0,0 +1,79 @@
+use std::sync::Arc;
+
+use refract::{
+    color::Color,
+    material::{Dielectric, Material, Matte, Metal, ReflectionType},
+    point::Point,
+    rng::random_range,
+    scene::{Scene, SceneBuilder},
+    sphere::Sphere,
+};
+
+pub struct Book1Scene;
+
+impl Book1Scene {
+    pub fn build(reflection_type: ReflectionType) -> Scene {
+        let mut scene_builder = SceneBuilder::new();
+
+        let ground = Sphere::new(
+            Point::new(0.0, -1000.0, 0.0),
+            1000.0,
+            Arc::new(Matte::new(Color::new(0.5, 0.5, 0.5), reflection_type)),
+        );
+
+        scene_builder.add_object(ground);
+
+        for a in -11..11 {
+            for b in -11..11 {
+                if let Some(sphere) = make_small_sphere(a, b, reflection_type) {
+                    scene_builder.add_object(sphere);
+                }
+            }
+        }
+
+        scene_builder.add_object(Sphere::new(
+            Point::new(0.0, 1.0, 0.0),
+            1.0,
+            Arc::new(Dielectric::new(1.5)),
+        ));
+        scene_builder.add_object(Sphere::new(
+            Point::new(-4.0, 1.0, 0.0),
+            1.0,
+            Arc::new(Matte::new(Color::new(0.4, 0.2, 0.1), reflection_type)),
+        ));
+        scene_builder.add_object(Sphere::new(
+            Point::new(4.0, 1.0, 0.0),
+            1.0,
+            Arc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0)),
+        ));
+
+        scene_builder.build()
+    }
+}
+
+fn make_small_sphere(a: i32, b: i32, reflection_type: ReflectionType) -> Option<Sphere> {
+    let center = Point::new(
+        a as f32 + 0.9 * random_range(0.0..1.0),
+        0.2,
+        b as f32 + 0.9 * random_range(0.0..1.0),
+    );
+
+    if (center - Point::new(4.0, 0.2, 0.0)).len() <= 0.9 {
+        return None;
+    }
+
+    let material: Arc<dyn Material> = match random_range(0.0..1.0) {
+        m if m < 0.8 => {
+            let albedo = Color::random() * Color::random();
+            Arc::new(Matte::new(albedo, reflection_type))
+        }
+        m if m < 0.95 => {
+            let albedo = Color::random();
+            let fuzz = random_range(0.0..0.5);
+            Arc::new(Metal::new(albedo, fuzz))
+        }
+        _ => Arc::new(Dielectric::new(1.5)),
+    };
+
+    Some(Sphere::new(center, 0.2, material))
+}
---
+++ refract-scenes/src/lib.rs
@@ -0,0 +1,3 @@
+pub mod book1;
+
+pub use book1::Book1Scene;
