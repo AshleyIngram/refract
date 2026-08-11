52cffbcdd423d7cf67ea55e05507abae7096487e
Ashley Ingram <ashley.ingram@outlook.com>
2026-08-11 21:59:55 +0100

    Cargo fmt

--- refract/src/lib.rs
+++ refract/src/lib.rs
@@ -3,8 +3,7 @@ pub mod render;
 pub mod surfaces;
 
 pub use core::{
-    bounding_box, bvh_node, color, direction, hittable, interval, point, ray, rng, scene,
-    sphere,
+    bounding_box, bvh_node, color, direction, hittable, interval, point, ray, rng, scene, sphere,
 };
 pub use render::{camera, canvas, pixel_buffer};
 pub use surfaces::material;
