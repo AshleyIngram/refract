df1ea2462ee7e3cd8d4c650c5f6228ebbaf5e871
Ashley Ingram <ashley.ingram@outlook.com>
2026-08-15 12:38:37 +0100

    Cargo fmt

--- refract-scenes/src/textures.rs
+++ refract-scenes/src/textures.rs
@@ -19,8 +19,8 @@ impl TexturesScene {
             CheckerTexture::from_colors(0.32, Color::new(0.2, 0.3, 0.1), Color::new(0.9, 0.9, 0.9));
         let material = Arc::new(Matte::with_texture(Arc::new(texture), reflection_type));
 
-        let sphere1 = Sphere::new_stationary(Point::new(0.0, -10.0, 0.0), 10.0, material.clone()); 
-        let sphere2 = Sphere::new_stationary(Point::new(0.0, 10.0, 0.0), 10.0, material.clone()); 
+        let sphere1 = Sphere::new_stationary(Point::new(0.0, -10.0, 0.0), 10.0, material.clone());
+        let sphere2 = Sphere::new_stationary(Point::new(0.0, 10.0, 0.0), 10.0, material.clone());
 
         scene_builder.add_object(sphere1);
         scene_builder.add_object(sphere2);
